fa14d8092d87e42bf7b61af78d5d45382421b321
Christophe Gigot <ch.gigot@gmail.com>
2017-08-17 21:02:33 +0200

    Initial commit.

---
+++ R/epiphy.R
@@ -0,0 +1,9 @@
+#------------------------------------------------------------------------------#
+#' \code{epiphy}: Explore and analyze observations of plant disease epidemics
+#'
+#' This package is intended primarily for the plant pathologist community.
+#'
+#' @docType package
+#' @name epiphy
+#------------------------------------------------------------------------------#
+NULL
