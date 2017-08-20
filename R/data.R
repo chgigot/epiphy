0e189dbfa90bad680d3c68c87725fb1eb3745c87
Christophe Gigot <ch.gigot@gmail.com>
2017-08-20 15:55:43 +0200

    Add data sets to test SADIE.

---
+++ R/data.R
@@ -0,0 +1,49 @@
+#------------------------------------------------------------------------------#
+#' Aphid counts
+#'
+#' TODO.
+#'
+#' @format A data frame with 63 rows and 3 variables:
+#' \tabular{lll}{
+#'     [, 1:2] \tab x,y \tab Spatial coordonates. \cr
+#'     [, 3]   \tab d   \tab Counts. \cr
+#' }
+#'
+#' @source Perry JN, Winder L, Holland JM, Alston RD. 1999. Red-blue plots for
+#'     detecting clusters in count data. Ecology Letters 2, 106-13.
+#'     \href{http://dx.doi.org/10.1046/j.1461-0248.1999.22057.x}{10.1046/j.1461-0248.1999.22057.x}
+#------------------------------------------------------------------------------#
+"aphid_counts"
+
+#------------------------------------------------------------------------------#
+#' Arthropods counts
+#'
+#' TODO.
+#'
+#' @format A data frame with 378 rows and 4 variables:
+#' \tabular{lll}{
+#'     [, 1]   \tab set \tab Grid id. \cr
+#'     [, 2:3] \tab x,y \tab Spatial coordonates. \cr
+#'     [, 4]   \tab d   \tab Counts. \cr
+#' }
+#'
+#' @source Holland JM, Winder L, Perry JN. 1999. Arthropod prey of farmland
+#'     birds: Their spatial distribution within a sprayed field with and without
+#'     buffer zones. Aspects of Applied Biology 54: 53-60.
+#------------------------------------------------------------------------------#
+"arthropods_counts"
+
+#------------------------------------------------------------------------------#
+# Codling moth counts
+#
+# @source Lavigne C, Ricci B, Franck P, Senoussi R. 2010. Spatial analyses of
+#     ecological count data: A density map comparison approach. Basic and
+#     Applied Ecology 11: 734-42.
+#     \href{http://dx.doi.org/10.1016/j.baae.2010.08.011}{10.1016/j.baae.2010.08.011}
+#------------------------------------------------------------------------------#
+#"codling_moth_counts"
+#
+
+
+
+
